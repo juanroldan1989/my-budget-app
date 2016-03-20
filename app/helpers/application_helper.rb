cbf3caa33fb4211606ec82d735d1b0ef2da32c96
Juan Roldan <juanroldan1989@gmail.com>
2016-03-20 18:27:43 +1300

    Deal price_text info formatted properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def deal_price_text(deal)
+    if deal.price.to_i > 0
+      "<b>#{deal.deal_type.camelize} deal for $#{deal.price}</b>".html_safe
+    else
+      "<b>#{deal.price_text.first}</b>".html_safe
+    end
+  end
 end
