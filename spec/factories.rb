f3f73060150f2ce2a49fd59248386c414ab0a860
Juan Roldan <juanroldan1989@gmail.com>
2016-03-26 23:43:41 +1300

    Application helper methods tested

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :deal do
     title     Faker::Commerce.product_name
     price     Faker::Number.between(1, 100)
-    deal_type ["single", "combined"].sample
+    deal_type "single"
   end
 end
---
+++ spec/helpers/application_helper.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe ApplicationHelper do
+  describe "#deal_price_text" do
+    describe "displays Deal's price with the right format" do
+      it "when deal has a price set" do
+        deal = create(:deal, price: 10)
+        # helper is an instance of ActionView::Base configured with the
+        # EventsHelper and all of Rails' built-in helpers
+        expect(helper.deal_price_text(deal)).to eq "<b>Single deal for $10.0</b>"
+      end
+
+      it "when deal doesn't have a price set" do
+        deal = create(:deal, price: 0, price_text: ["Adults only"])
+        expect(helper.deal_price_text(deal)).to eq "<b>Adults only</b>"
+      end
+    end
+  end
+end
