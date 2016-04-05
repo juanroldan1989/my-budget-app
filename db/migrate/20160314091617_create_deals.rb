fddb867366d93ebda2565603e7ace05f343bf8ba
Juan Roldan <juanroldan1989@gmail.com>
2016-04-05 19:53:38 +1200

    Deal table - Location's latitude and longitude fields setup

--- db/migrate/20160314091617_create_deals.rb
+++ db/migrate/20160314091617_create_deals.rb
@@ -3,15 +3,17 @@ class CreateDeals < ActiveRecord::Migration
     create_table :deals do |t|
       t.string  :title
       t.string  :slug
-      t.text    :description, array: true, default: []
+      t.text    :description,   array: true, default: []
       t.string  :deal_type
-      t.decimal :min_price,   precision: 10, scale: 2, default: 0.0
-      t.decimal :max_price,   precision: 10, scale: 2, default: 0.0
-      t.decimal :price,       precision: 10, scale: 2, default: 0.0
-      t.text    :price_text,  array: true, default: []
-      t.text    :image_urls,  array: true, default: []
-      t.text    :links,       array: true, default: []
-      t.text    :keywords,    array: true, default: []
+      t.decimal :min_price,     precision: 10, scale: 2, default: 0.0
+      t.decimal :max_price,     precision: 10, scale: 2, default: 0.0
+      t.decimal :price,         precision: 10, scale: 2, default: 0.0
+      t.text    :price_text,    array: true, default: []
+      t.text    :image_urls,    array: true, default: []
+      t.text    :links,         array: true, default: []
+      t.text    :keywords,      array: true, default: []
+      t.decimal :location_lat,  precision: 10, scale: 2, default: 0.0
+      t.decimal :location_long, precision: 10, scale: 2, default: 0.0
 
       t.timestamps
     end
@@ -24,5 +26,7 @@ class CreateDeals < ActiveRecord::Migration
     add_index :deals, :price
     add_index :deals, :price_text
     add_index :deals, :keywords
+    add_index :deals, :location_lat
+    add_index :deals, :location_long
   end
 end
